5cb86b873f808ec3143e009ca7770c122293aafc
daliborstakic <stakicdalibor64@gmail.com>
2020-08-26 14:36:17 +0200

    removed old test and added new rent_bike test. test now checks for ValueError

--- test_shop.py
+++ test_shop.py
@@ -14,11 +14,6 @@ class ShopTest(unittest.TestCase):
         self.assertEqual(self.shop2.display_stock(), 10)
         self.assertEqual(self.shop3.display_stock(), 1)
 
-    def test_rent_bike(self):
-        now = datetime.now()
-        self.assertEqual(self.shop1.rentBike(), (now, 1, 1))
-        self.assertEqual(self.shop2.rentBike(2, 3), (now, 2, 3))
-
     def test_stock_change(self):
         self.assertEqual(self.shop1.display_stock(), 1)
         self.shop1.rentBike()
@@ -26,6 +21,13 @@ class ShopTest(unittest.TestCase):
         self.assertEqual(self.shop2.display_stock(), 10)
         self.shop2.rentBike()
         self.assertEqual(self.shop2.display_stock(), 9)
+
+    def test_rent_bike(self):
+        now = datetime.now()
+        self.assertEqual(self.shop1.rentBike(), (now, 1, 1))
+        self.assertEqual(self.shop2.rentBike(2, 2), (now, 2, 2))
+        with self.assertRaises(ValueError):
+            self.shop3.rentBike(-1, 2)
 
 
 if __name__ == '__main__':
