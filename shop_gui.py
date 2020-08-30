1f6363ab47cb5a639175370d8e6b1c2457773915
daliborstakic <stakicdalibor64@gmail.com>
2020-08-30 09:23:37 +0200

    added two buttons

--- shop_gui.py
+++ shop_gui.py
@@ -10,12 +10,17 @@ optionList = ['Hourly', 'Daily', 'Weekly']
 variable = tk.StringVar(root)
 variable.set(optionList[0])
 
-# GUI Elements
+# First row
 bike_label = tk.Label(root, text="Enter amount of bikes:")
 bike_entry = tk.Entry(root)
 
+# Second row
 rental_label = tk.Label(root, text="Choose rental basis:")
 rental_list = tk.OptionMenu(root, variable, *optionList)
+
+# Third row*
+rent_button = tk.Button(root, text='Rent')
+return_button = tk.Button(root, text='Return', state=tk.DISABLED)
 
 # Display elements
 bike_label.grid(row=0, column=0)
@@ -23,6 +28,11 @@ bike_entry.grid(row=0, column=1)
 
 rental_label.grid(row=1, column=0)
 rental_list.grid(row=1, column=1)
+
+# *It's the fourth row because a label will be inserted later at
+# the actual third row
+rent_button.grid(row=3, column=0)
+return_button.grid(row=3, column=1)
 
 # Main loop
 root.mainloop()
