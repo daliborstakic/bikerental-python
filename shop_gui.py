3ec40cd1591b6d4203335e38bf22e1bfdec015f3
daliborstakic <stakicdalibor64@gmail.com>
2020-08-30 09:02:38 +0200

    added first row of elements

--- shop_gui.py
+++ shop_gui.py
@@ -3,5 +3,13 @@ import tkinter as tk
 # Root window
 root = tk.Tk()
 
+# GUI Elements
+bike_label = tk.Label(root, text="Enter amount of bikes:")
+bike_entry = tk.Entry(root)
+
+# Display elements
+bike_label.grid(row=0, column=0)
+bike_entry.grid(row=0, column=1)
+
 # Main loop
 root.mainloop()
